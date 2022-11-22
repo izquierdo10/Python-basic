902bd4acb80c85056a7bad3be8909b0bf46f8670
ADIC <84812911+izquierdo10@users.noreply.github.com>
2022-11-22 15:45:38 -0500

    Update settings.py

--- 4Expert/proyecto1/proyecto1/settings.py
+++ 4Expert/proyecto1/proyecto1/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x9-e$#43gb-u+u+5%6o07bvzsy#(rq@sh##g^mo0((*&iv&f=4'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
